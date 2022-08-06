b2d86c1fa43779b0ec269fc7917667813f8df88e
mozammil0218 <104901574+mozammil0218@users.noreply.github.com>
2022-08-06 22:10:09 +0530

    input image update

--- components/Input.js
+++ components/Input.js
@@ -43,12 +43,14 @@ const Input = () => {
         const imageRef = ref(storage, `posts/${docRef.id}/image`);
         
         if (selectedFile) {
-            await uploadedString(imageRef, selectedFile, "data_url").then(async () => {
-                 const downloadURL = await getDownloadURL(imageRef);
-                 await updateDoc(doc(db, "posts", docRef.id), {
-                   image: downloadURL,
-                 });
-            })
+          await uploadString(imageRef, selectedFile, "data_url").then(
+            async () => {
+              const downloadURL = await getDownloadURL(imageRef);
+              await updateDoc(doc(db, "posts", docRef.id), {
+                image: downloadURL,
+              });
+            }
+          );
         }
         
             setLoading(false);
